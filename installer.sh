ed75409d1168e13d76e4853fd7e4266aaaf28d0d
Erik Karsten <erik@opcode.xyz>
2025-03-07 14:27:44 +0000

    feat: get it to "run"

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ docker tag ghcr.io/siderolabs/countercept:wip "$COUNTERCEPT_IMAGE"
 docker push "$COUNTERCEPT_IMAGE"
 
 docker run --rm -t -v "$HOME/.docker/config.json:/docker-config/config.json" -e DOCKER_CONFIG="/docker-config" -v /dev:/dev --privileged -v "$PWD/_out:/out" "ghcr.io/siderolabs/imager:${TALOS_VERSION}" --arch "${ARCH}" \
-  --system-extension-image ${COUNTERCEPT_IMAGE}  ${PROFILE}
+  --system-extension-image ${COUNTERCEPT_IMAGE} --system-extension-image "ghcr.io/siderolabs/glibc:2.40"  ${PROFILE}
 docker load -i ./_out/installer-${ARCH}.tar 
 docker tag ghcr.io/siderolabs/installer:${TALOS_VERSION} dev.artifactor.ee/talos-countercept-installer:${TALOS_VERSION}
 docker push dev.artifactor.ee/talos-countercept-installer:${TALOS_VERSION}
